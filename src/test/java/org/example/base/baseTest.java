adb589ba2963b6739f25b094f80b6183be41c02e
Abhishek <rishut195@gmail.com>
2024-06-19 17:20:07 +0530

    small changes

--- src/test/java/org/example/base/baseTest.java
+++ src/test/java/org/example/base/baseTest.java
@@ -1,4 +1,29 @@
 package org.example.base;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.example.modules.PayloadManager;
+import org.testng.annotations.BeforeMethod;
+
 public class baseTest {
+
+    public RequestSpecification requestSpecification;
+    public ValidatableResponse validatableResponse;
+    public AssertionError assertionError;
+    public PayloadManager payloadManager;
+    public Response response;
+
+
+    @BeforeMethod
+    public void setUpConfig() {
+        payloadManager = new PayloadManager();
+        requestSpecification = RestAssured.given();
+
+
+
+
+    }
 }
